faf1b7cc352099092d1fd3f2ffc9f3fc1eb69a00
sujay-k <kulkarnisujayk@gmail.com>
2019-02-15 15:32:45 +0530

    yeah! started

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[]){
+
+    
+    
+    return 0;
+
+}
