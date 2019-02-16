3c289210510b86431ba5816d2b6805e96d945818
sujay-k <kulkarnisujayk@gmail.com>
2019-02-16 12:21:47 +0530

    imageRead and imageShow

--- main.cpp
+++ main.cpp
@@ -5,13 +5,57 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <iostream>
+#include <vector>
+#include <glob.h>
+
+#define FOR(begin,limit) for(auto i=begin; i<limit; i++)
 
 using namespace std;
 using namespace cv;
 
-int main(int argc, char* argv[]){
+void imShow(vector<string> fileName){
 
+    int numberOfImages = fileName.size();
+    vector<Mat> src;
+
+    cout << "Reading " << numberOfImages << " number of images..."<<endl;
+    FOR(0,numberOfImages){
+        src.push_back(imread(fileName[i]));
+    }
+
+    while(true){
+        FOR(0,numberOfImages){
+            imshow(fileName[i],src[i]);
+        }
+
+        int k = waitKey(1);
+        if(k==27) return;
+    }
+
+}
+
+vector<string> readImages(string path){
     
+    glob_t r;
+    vector<string> res;
+    
+    path = path+"/*";
+    glob(path.c_str(), GLOB_TILDE, NULL, &r);
+    
+    FOR(0,r.gl_pathc){
+        res.push_back(r.gl_pathv[i]);
+    }
+
+    return res;
+
+}
+
+int main(int argc, char* argv[]){
+    
+    string path = "./test_images";
+    
+    vector<string> fileName = readImages(path);
+    imShow(fileName);
     
     return 0;
 
