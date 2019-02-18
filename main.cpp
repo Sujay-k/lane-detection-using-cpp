b513142821c8400abf53360b1a6c94a2133872c3
sujay-k <kulkarnisujayk@gmail.com>
2019-02-18 15:59:01 +0530

    LOL i started doin

--- main.cpp
+++ main.cpp
@@ -13,20 +13,31 @@
 using namespace std;
 using namespace cv;
 
-void imShow(vector<string> fileName){
+// Thresholds 
+const int minThreshold = 50;
+const int maxThreshold = 200;
+const int kernelSize = 3;
 
-    int numberOfImages = fileName.size();
+vector<Mat> stringToMat(vector<string> fileName){
+
     vector<Mat> src;
 
-    cout << "Reading " << numberOfImages << " number of images..."<<endl;
-    FOR(0,numberOfImages){
-        src.push_back(imread(fileName[i]));
-    }
+    FOR(0,fileName.size())  src.push_back(imread(fileName[i],0));
+
+    return src;
+
+}
+
+void imShow(vector<string> fileName){
+
+    vector<Mat> src;
+
+    cout << "Reading " << fileName.size() << " number of images..."<<endl;
+    src = stringToMat(fileName);
 
     while(true){
-        FOR(0,numberOfImages){
-            imshow(fileName[i],src[i]);
-        }
+
+        FOR(0,fileName.size())  imshow(fileName[i],src[i]);
 
         int k = waitKey(1);
         if(k==27) return;
@@ -42,21 +53,52 @@ vector<string> readImages(string path){
     path = path+"/*";
     glob(path.c_str(), GLOB_TILDE, NULL, &r);
     
-    FOR(0,r.gl_pathc){
-        res.push_back(r.gl_pathv[i]);
-    }
+    FOR(0,r.gl_pathc)   res.push_back(r.gl_pathv[i]);
 
     return res;
 
 }
+
+void processOne(Mat src){
+
+    Canny(src, src, minThreshold, maxThreshold, kernelSize);
+    imshow("Edge Map", src);
+
+    int k = waitKey(0); 
+    if(k==27) return;
+
+}
+
+bool processImages(vector<string> fileName){
+
+    vector<Mat> src = stringToMat(fileName);
+
+    FOR(0,fileName.size()){
+        try{
+            processOne(src[i]);
+        }
+        catch(...){
+            return false;
+        }
+
+    }
+
+    return true;
+
+}
+
 
 int main(int argc, char* argv[]){
     
     string path = "./test_images";
     
     vector<string> fileName = readImages(path);
-    imShow(fileName);
-    
+
+    bool state = processImages(fileName);
+
+    if(state){ cout << "Images processed correctly!" << endl;}
+    else{ cout << "Error: Watch out, man! look back at your code." << endl; return -1;}
+
     return 0;
 
 }
